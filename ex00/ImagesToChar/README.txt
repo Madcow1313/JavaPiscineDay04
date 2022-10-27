74ef323ad204104a34f09e8b0b5deabe8038928d
Madcow1313 <safbul@gmail.com>
2022-10-27 18:26:50 +0300

    ex00 done

---
+++ ex00/ImagesToChar/README.txt
@@ -0,0 +1,3 @@
+#makedir ./ex00/ImagesToChar/target
+javac -d .\ex00\ImagesToChar\target\ .\ex00\ImagesToChar\src\edu\school21\printer\app\App.java .\ex00\ImagesToChar\src\edu\school21\printer\logic\BmpReader.java
+java -cp .\ex00\ImagesToChar\target edu.school21.printer.app.App
---
+++ ex00/ImagesToChar/src/edu/school21/printer/app/App.java
@@ -0,0 +1,17 @@
+package edu.school21.printer.app;
+
+import edu.school21.printer.logic.BmpReader;
+
+public class App {
+    public static void main(String[] args){
+        try {
+            BmpReader bmpReader = new BmpReader("./ex00/ImagesToChar/it.bmp");
+            bmpReader.readBmpTo2DArray();
+            bmpReader.drawBmp();
+        }
+        catch (Exception e){
+            System.err.println("Something went wrong\nCaught exception: " + e);
+            System.exit(-1);
+        }
+    }
+}
